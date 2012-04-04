a8219b8bfdab0bfd7a3db77d1e2a02c978ddcc89
Josh W <josher19@users.sf.net>
2012-04-05 00:38:02 +0800

    CLI

--- npm-search-cli.js
+++ npm-search-cli.js
@@ -1,8 +1,14 @@
 npmsearch = require('./index.js')
+if (process.argv.length < 3) {
+    var program = process.env['_'] || process.argv[1] ;
+    process.stdout.write("Usage: " + program + " searchTerm [command] \nWhere command is search (default), keywords, readme, details, or author\n");
+    process.stdout.write("Examples:\n  npmsearch npm\n  npmsearch Josh author\n  npmsearch api kw\n");
+    process.exit(-1);
+}
 var shortcut = {'d': 'details', 'a':'author', 'kw':'keywords', 'au':'author', 'k':'keywords'}
 function kword(a) { return shortcut[a] || a }
 var search = npmsearch[kword(process.argv[3])] || npmsearch
 console.info(process.argv.join(" ") + "\n")
-var res=search(process.argv[2] || "npm")
+var res=search(process.argv[2])
 console.info(npmsearch.num().join("\n") || res)
 // console.info(res)
